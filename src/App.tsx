9da430760fc5b090d5b8881833b26b32b4574bc5
thayrone <thayronedepaula15@gmail.com>
2021-09-07 09:59:03 -0300

    feat: header logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography } from "@material-ui/core";
+import Header from "./Components/Header";
 
-const useStyles = makeStyles((theme) => ({
-  
-}));
+const useStyles = makeStyles((theme) => ({}));
 const App = () => {
   const classes = useStyles();
   return (
     <Container maxWidth="xl">
-      <Typography
-        component="div"
-        style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
-      />
+      <Header />
     </Container>
   );
 };
