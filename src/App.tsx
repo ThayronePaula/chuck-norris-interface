e3b37ed292a604372a55bf94d3859e06bbb96049
thayrone <thayronedepaula15@gmail.com>
2021-09-09 11:08:18 -0300

    add random and joke list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Typography } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import Header from "./Components/Header";
+import JokeList from "./Components/JokeList";
+import RandomJoke from "./Components/RandomJoke";
 
 const useStyles = makeStyles((theme) => ({}));
+interface CategoriesJoke {
+  type: string;
+  value: string[];
+}
 const App = () => {
   const classes = useStyles();
+  const [categories, setCategories] = React.useState<CategoriesJoke>();
+  React.useEffect(() => {
+    const getChuckNorrisJokes = async () => {
+      try {
+        const response = await fetch("http://api.icndb.com/categories");
+        const json = await response.json();
+        setCategories(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getChuckNorrisJokes();
+  }, []);
   return (
     <Container maxWidth="xl">
       <Header />
+      <RandomJoke categories={categories?.value} />
+      <JokeList categories={categories?.value} />
     </Container>
   );
 };
